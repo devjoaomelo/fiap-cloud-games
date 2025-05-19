5d5784877798d7736ce0ceb728c50bd928e7dbaa
devjoaomelo <devjoaomelo@gmail.com>
2025-05-19 16:14:22 -0300

    first migration done

---
+++ src/FCG.Infra/Factories/FCGDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using FCG.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FCG.Infra
+{
+    public class FCGDbContextFactory : IDesignTimeDbContextFactory<FCGDbContext>
+    {
+        public FCGDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FCGDbContext>();
+            optionsBuilder.UseMySql(
+                "server=localhost;database=FCGDb;user=admin;password=admin",
+                new MySqlServerVersion(new Version(8, 0, 41))
+            );
+
+            return new FCGDbContext(optionsBuilder.Options);
+        }
+    }
+}
