83c359b5ecd4222bbaa3b9b747bf0f8133e70d63
devjoaomelo <devjoaomelo@gmail.com>
2025-05-21 19:03:15 -0300

    Tests ValueObjects

---
+++ src/FCG.Tests/ValueObjects/EmailTests.cs
@@ -0,0 +1,26 @@
+﻿using FCG.Domain.ValueObjects;
+using Xunit;
+
+namespace FCG.Tests.ValueObjects
+{
+    public class EmailTests
+    {
+        [Fact]
+        public void CreateValidEmail()
+        {
+            var email = new Email("user@gmail.com");
+            Assert.Equal("user@gmail.com", email.Address);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("invalid@email")]
+        [InlineData("user@.com")]
+        public void CreateInvalidEmail(string email)
+        {
+            Assert.Throws<ArgumentException>(() => new Email(email));
+        }
+
+    }
+}
---
+++ src/FCG.Tests/ValueObjects/PasswordTests.cs
@@ -0,0 +1,42 @@
+﻿using FCG.Domain.ValueObjects;
+using Xunit;
+
+namespace FCG.Tests.ValueObjects
+{
+    public class PasswordTests
+    {
+        [Fact]
+        public void CreateValidPassword()
+        {
+            var password = new Password("StrongP4ss!");
+            Assert.False(string.IsNullOrWhiteSpace(password.Hash));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("8756")]
+        public void InvalidPasswordEx(string invalidPass)
+        {
+            Assert.Throws<ArgumentException>(() => new Password(invalidPass));
+        }
+
+        [Fact]
+        public void PasswordMustBeEqual()
+        {
+            var pass1 = new Password("pass1234!");
+            var pass2 = new Password("pass1234!");
+
+            Assert.Equal(pass1, pass2);
+        }
+
+        [Fact]
+        public void PasswordMustBeDifferent()
+        {
+            var pass1 = new Password("pass1234!");
+            var pass2 = new Password("pass4321!");
+
+            Assert.NotEqual(pass1, pass2);
+        }
+    }
+}
