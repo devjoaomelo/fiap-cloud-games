302cb158d58137781c1a7f29a346cafa87a065e7
devjoaomelo <devjoaomelo@gmail.com>
2025-05-21 15:31:42 -0300

    GetUserByEmail UseCase

---
+++ src/FCG.Application/UseCases/Users/GetUserByEmail/GetUserByEmailRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace FCG.Application.UseCases.Users.GetUserByEmail
+{
+    public class GetUserByEmailRequest
+    {
+        public string Email { get; set; }
+        public GetUserByEmailRequest(string email)
+        {
+            Email = email;
+        }
+    }
+
+}
---
+++ src/FCG.Application/UseCases/Users/GetUserByEmail/GetUserByEmailResponse.cs
@@ -0,0 +1,26 @@
+﻿using FCG.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCG.Application.UseCases.Users.GetUserByEmail
+{
+    public class GetUserByEmailResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Profile Profile { get; set; }
+
+        public GetUserByEmailResponse(Guid id, string name, string email, Profile profile)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Profile = profile;
+
+        }
+    }
+}
