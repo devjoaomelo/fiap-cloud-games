bebbf017227aa7329a26de946ed531e093edec91
devjoaomelo <devjoaomelo@gmail.com>
2025-05-26 13:35:57 -0300

    Update GetUserByIdHandler.cs

--- src/FCG.Application/UseCases/Users/GetUserById/GetUserByIdHandler.cs
+++ src/FCG.Application/UseCases/Users/GetUserById/GetUserByIdHandler.cs
@@ -1,23 +1,23 @@
 ﻿using FCG.Domain.Interfaces;
 
-namespace FCG.Application.UseCases.Users.GetUserById
+namespace FCG.Application.UseCases.Users.GetUserById;
+
+public class GetUserByIdHandler
 {
-    public class GetUserByIdHandler
+    private readonly IUserRepository _userRepository;
+    public GetUserByIdHandler(IUserRepository userRepository)
     {
-        private readonly IUserRepository _userRepository;
-        public GetUserByIdHandler(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
+        _userRepository = userRepository;
+    }
 
-        public async Task<GetUserByIdResponse> HandleGetUserByIdAync(GetUserByIdRequest request)
-        {
-            var user = await _userRepository.GetUserByIdAsync(request.Id);
+    public async Task<GetUserByIdResponse> HandleGetUserByIdAsync(GetUserByIdRequest request)
+    {
+        var user = await _userRepository.GetUserByIdAsync(request.Id);
 
-            if (user == null)
-                return null;
+        if (user == null)
+            throw new InvalidOperationException("User not found.");
 
-            return new GetUserByIdResponse(user.Id, user.Name, user.Email.ToString(), user.Profile.ToString());
-        }
+        return new GetUserByIdResponse(user.Id, user.Name, user.Email.ToString(), user.Profile.ToString());
     }
 }
+
