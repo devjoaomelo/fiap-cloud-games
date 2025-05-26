e1fbac2e13cc8ac9ca6045f281409f4b37fc4de3
devjoaomelo <devjoaomelo@gmail.com>
2025-05-26 13:39:00 -0300

    Update EmailTests.cs

--- src/FCG.Tests/ValueObjects/EmailTests.cs
+++ src/FCG.Tests/ValueObjects/EmailTests.cs
@@ -1,26 +1,25 @@
 ﻿using FCG.Domain.ValueObjects;
 using Xunit;
 
-namespace FCG.Tests.ValueObjects
+namespace FCG.Tests.ValueObjects;
+
+public class EmailTests
 {
-    public class EmailTests
+    [Fact]
+    public void CreateValidEmail()
     {
-        [Fact]
-        public void CreateValidEmail()
-        {
-            var email = new Email("user@gmail.com");
-            Assert.Equal("user@gmail.com", email.Address);
-        }
+        var email = new Email("user@gmail.com");
+        Assert.Equal("user@gmail.com", email.Address);
+    }
 
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("invalid@email")]
-        [InlineData("user@.com")]
-        public void CreateInvalidEmail(string email)
-        {
-            Assert.Throws<ArgumentException>(() => new Email(email));
-        }
-
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("invalid@email")]
+    [InlineData("user@.com")]
+    public void CreateInvalidEmail(string email)
+    {
+        Assert.Throws<ArgumentException>(() => new Email(email));
     }
 }
+
