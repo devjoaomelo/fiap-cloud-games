0cbfc03a6cd7b89c4fd79f757acdd1ab36b823ac
João Melo <devjoaomelo@gmail.com>
2025-07-29 15:40:33 -0300

    Delete game refatorado

--- src/FCG.Application/UseCases/Games/DeleteGame/DeleteGameHandler.cs
+++ src/FCG.Application/UseCases/Games/DeleteGame/DeleteGameHandler.cs
@@ -1,27 +1,25 @@
-using FCG.Application.UseCases.Users.DeleteUser;
 using FCG.Domain.Interfaces;
+using FCG.Domain.Services;
 
 namespace FCG.Application.UseCases.Games.DeleteGame;
 
 public class DeleteGameHandler
 {
     private readonly IGameRepository _gameRepository;
+    private readonly GameValidationService _gameValidationService;
 
     public DeleteGameHandler(IGameRepository gameRepository)
     {
         _gameRepository = gameRepository;
+        _gameValidationService = new GameValidationService(_gameRepository);
     }
 
     public async Task<DeleteGameResponse> HandleDeleteGameAsync(DeleteGameRequest request)
     {
         var game = await _gameRepository.GetGameByIdAsync(request.Id);
-
-        if (game is null)
-        {
-            throw new InvalidOperationException("Game not found.");
-        }
-
+        
         await _gameRepository.DeleteGameAsync(game);
+        
         return new DeleteGameResponse(true, "Game deleted");
     }
 }
---
+++ src/FCG.Domain/Services/GameValidationService.cs
@@ -0,0 +1,27 @@
+﻿using FCG.Domain.Entities;
+using FCG.Domain.Interfaces;
+
+namespace FCG.Domain.Services;
+
+public class GameValidationService
+{
+    private readonly IGameRepository _gameRepository;
+
+    public GameValidationService(IGameRepository gameRepository)
+    {
+        _gameRepository = gameRepository;
+    }
+
+    public async Task<Game> GetGameIfExistsAsync(Guid gameId)
+    {
+        if (gameId == Guid.Empty)
+            throw new ArgumentException("Invalid game ID.", nameof(gameId));
+
+        var game = await _gameRepository.GetGameByIdAsync(gameId);
+
+        if (game is null)
+            throw new InvalidOperationException("Game not found.");
+
+        return game;
+    }
+}
